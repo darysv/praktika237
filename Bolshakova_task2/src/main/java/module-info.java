bf1097fba35245215cf2125bf6aaa403097c045b
darysv <bolshakovad07@gmail.com>
2024-12-18 22:02:09 +0300

    Delete Bolshakova_task2 directory

--- Bolshakova_task2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.bolshakova.bolshakova_task2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.bolshakova.bolshakova_task2 to javafx.fxml;
-    exports ru.bolshakova.bolshakova_task2;
-}
